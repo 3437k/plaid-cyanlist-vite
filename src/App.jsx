97156534d728d2ebdc0430ec3ef7cb6960190799
KwonHyukJin <1113437k@naver.ocm>
2022-11-30 22:47:46 +0900

    feat : color change button

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import styled from 'styled-components'
+
+const Tag = styled.div`
+  position: relative;  
+  color: white;
+  background:${props => (props.tagColor)};
+  padding: 20px 22px;
+  border-radius: 24px;
+  font-size: 2em;
+  transition: background-color 1s linear
+`
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [tagColor, setTagColor] = useState('red')
+  
+  setTimeout(() => {
+    const randomColor ='#' + Math.floor(Math.random()*16777215).toString(16);
+    setTagColor(randomColor);    
+  }, 2000);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <h1 style={{ color: {tagColor} }}>Here</h1> */}
+      <Tag tagColor={tagColor}>Here</Tag>      
     </div>
   )
 }
